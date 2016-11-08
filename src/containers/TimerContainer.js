aa8015600d5c73ad0015e2f6ee6da8211e284bf9
Margarita Klubochkina <gretchenfitze@gmail.com>
2016-11-09 01:43:19 +0300

    Fix actions dispatching in timer container

--- src/containers/TimerContainer.js
+++ src/containers/TimerContainer.js
@@ -1,5 +1,4 @@
 import React, { PropTypes } from 'react';
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import * as actions from '../actions/TimerActions';
@@ -12,30 +11,30 @@ import notify from '../utilities/notify';
 let timerId;
 
 class TimerContainer extends React.Component {
-  constructor({ actions, params }) {
+  constructor({ params, onTimerReset }) {
     super();
-    actions.resetTimer(params.timerType || 'work');
+    onTimerReset(params.timerType || 'work');
     Notification.requestPermission();
   }
 
-  componentWillReceiveProps({ params, actions }) {
+  componentWillReceiveProps({ params, onTimerReset }) {
     if (this.props.params.timerType !== params.timerType) {
       clearInterval(timerId);
-      actions.resetTimer(params.timerType);
+      onTimerReset(params.timerType);
     }
   }
 
-  componentDidUpdate({ seconds, timerType, actions }) {
+  componentDidUpdate({ seconds, timerType, onTimerReset }) {
     if (!seconds) {
       clearInterval(timerId);
       notify(timerType);
-      actions.resetTimer(timerType);
+      onTimerReset(timerType);
     }
   }
 
   render() {
-    const { seconds, active, timerType, onStartClick, onFormInput,
-      startingTime, settingsVisibility, actions } = this.props;
+    const { seconds, active, timerType, startingTime, settingsVisibility,
+      onStartClick, onFormInput, onSettingsClick } = this.props;
     return (
       <div>
         <TimerTypes />
@@ -48,7 +47,7 @@ class TimerContainer extends React.Component {
         <Controls
           active={active}
           onStartClick={event => onStartClick(event, active)}
-          onSettingsClick={() => actions.toggleSettings()}
+          onSettingsClick={() => onSettingsClick()}
         />
       </div>
     );
@@ -56,30 +55,31 @@ class TimerContainer extends React.Component {
 }
 
 TimerContainer.propTypes = {
+  params: PropTypes.object.isRequired,
   seconds: PropTypes.number.isRequired,
   active: PropTypes.bool.isRequired,
-  settingsVisibility: PropTypes.bool.isRequired,
   timerType: PropTypes.string.isRequired,
   startingTime: PropTypes.object.isRequired,
+  settingsVisibility: PropTypes.bool.isRequired,
   onStartClick: PropTypes.func.isRequired,
   onFormInput: PropTypes.func.isRequired,
-  actions: PropTypes.object.isRequired,
-  params: PropTypes.object.isRequired,
+  onSettingsClick: PropTypes.func.isRequired,
+  onTimerReset: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state, { params }) => {
   const { seconds, active, startingTime, settingsVisibility } = state.timer;
   const { timerType } = params || 'work';
-  return { timerType, seconds, active, startingTime, settingsVisibility };
+  return { seconds, active, timerType, startingTime, settingsVisibility };
 };
 
 const mapDispatchToProps = dispatch => ({
-  actions: bindActionCreators(actions, dispatch),
   onStartClick(event, active) {
     dispatch(actions.toggleTimer());
     if (!active) timerId = setInterval(() => { dispatch(actions.timerTick()); }, 1000);
     else clearInterval(timerId);
   },
+
   onFormInput(event, timerType, startingTime) {
     event.preventDefault();
     clearInterval(timerId);
@@ -87,6 +87,14 @@ const mapDispatchToProps = dispatch => ({
       ...startingTime,
       [event.target.id]: event.target.value * 60,
     }));
+    dispatch(actions.resetTimer(timerType));
+  },
+
+  onSettingsClick() {
+    dispatch(actions.toggleSettings());
+  },
+
+  onTimerReset(timerType) {
     dispatch(actions.resetTimer(timerType));
   },
 });
