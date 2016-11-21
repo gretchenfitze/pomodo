99362ec876bdc06a22f340783dc6dad812d29f50
gretchenfitze <gretchenfitze@gmail.com>
2016-11-21 21:37:27 +0300

    Add redux store test

--- src/components/Timer/Timer.spec.jsx
+++ src/components/Timer/Timer.spec.jsx
@@ -7,16 +7,23 @@ import Timer from './Timer.jsx';
 expect.extend(expectJSX);
 
 describe('Timer', () => {
-  it('should render the timer', () => {
+  function renderTimer(min = 0, sec = 0) {
     const renderer = ReactTestUtils.createRenderer();
-    renderer.render(<Timer minutes={4} seconds={20} />);
-    const result = renderer.getRenderOutput();
-    expect(result.type).toBe('div');
-    const expected = (
-      <div className={undefined}>
-        04:20
-      </div>
-    );
-    expect(result).toEqualJSX(expected);
+    renderer.render(<Timer minutes={min} seconds={sec} />);
+    return renderer.getRenderOutput();
+  }
+
+  it('should render a div', () => {
+    expect(renderTimer().type).toBe('div');
+  });
+
+  it('should not add 0 if it is not necessary', () => {
+    expect(renderTimer(12, 35)).toIncludeJSX('12:35');
+    expect(renderTimer(16, 59)).toIncludeJSX('16:59');
+  });
+
+  it('should add 0 if it is necessary', () => {
+    expect(renderTimer(0, 0)).toIncludeJSX('00:00');
+    expect(renderTimer(4, 2)).toIncludeJSX('04:02');
   });
 });
---
+++ src/store/store.spec.jsx
@@ -0,0 +1,72 @@
+import expect from 'expect';
+import configureStore from './configureStore';
+import { RESET, SET_TIMER, START, STOP, TICK, TOGGLE_SETTINGS } from '../constants/TimerActionTypes';
+
+describe('store', () => {
+  const store = configureStore();
+
+  it('should initialize', () => {
+    const actual = store.getState();
+    const expected = {
+      timer: {
+        startingTime: {
+          work: 1500,
+          shortBreak: 300,
+          longBreak: 900,
+        },
+        seconds: 1,
+      },
+    };
+    expect(actual).toEqual(expected);
+  });
+
+  it('should work with the series of actions', () => {
+    const actions = [
+      {
+        type: TOGGLE_SETTINGS,
+      },
+      {
+        type: SET_TIMER,
+        startingTime: {
+          work: 150,
+          shortBreak: 30,
+          longBreak: 90,
+        },
+      },
+      {
+        type: RESET,
+        timerType: 'work',
+      },
+      {
+        type: START,
+        active: 1,
+      },
+      {
+        type: TICK,
+      },
+      {
+        type: TICK,
+      },
+      {
+        type: STOP,
+      },
+    ];
+
+    actions.forEach(action => store.dispatch(action));
+    const actual = store.getState();
+    const expected = {
+      timer: {
+        startingTime: {
+          work: 150,
+          shortBreak: 30,
+          longBreak: 90,
+        },
+        seconds: 148,
+        settingsVisibility: true,
+        active: null,
+      },
+    };
+
+    expect(actual).toEqual(expected);
+  });
+});
