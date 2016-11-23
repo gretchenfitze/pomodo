6af8e5d1fd158ba1c0d8059345212d067d086801
gretchenfitze <gretchenfitze@gmail.com>
2016-11-24 01:06:53 +0300

    Add timer reducer test

---
+++ src/reducers/timer/timer.spec.jsx
@@ -0,0 +1,123 @@
+import expect from 'expect';
+import { RESET, SET_TIMER, START, STOP, TICK, TOGGLE_SETTINGS } from '../../constants/TimerActionTypes';
+import reducer from './timer';
+
+describe('Timer reducer', () => {
+  function stateBefore() {
+    return {
+      startingTime: {
+        work: 1500,
+        shortBreak: 300,
+        longBreak: 900,
+      },
+      seconds: 100,
+    };
+  }
+
+  it('handles timer reset', () => {
+    const action = {
+      type: RESET,
+      timerType: 'work',
+    };
+    const expected = {
+      startingTime: {
+        work: 1500,
+        shortBreak: 300,
+        longBreak: 900,
+      },
+      seconds: 1500,
+      active: null,
+    };
+    const actual = reducer(stateBefore(), action);
+    expect(actual).toEqual(expected);
+  });
+
+  it('handles setting timer', () => {
+    const action = {
+      type: SET_TIMER,
+      startingTime: {
+        work: 2000,
+        shortBreak: 300,
+        longBreak: 750,
+      },
+    };
+    const expected = {
+      startingTime: {
+        work: 2000,
+        shortBreak: 300,
+        longBreak: 750,
+      },
+      seconds: 100,
+    };
+    const actual = reducer(stateBefore(), action);
+    expect(actual).toEqual(expected);
+  });
+
+  it('handles timer start', () => {
+    const action = {
+      type: START,
+      active: 420,
+    };
+    const expected = {
+      startingTime: {
+        work: 1500,
+        shortBreak: 300,
+        longBreak: 900,
+      },
+      seconds: 100,
+      active: 420,
+    };
+    const actual = reducer(stateBefore(), action);
+    expect(actual).toEqual(expected);
+  });
+
+  it('handles timer stop', () => {
+    const action = {
+      type: STOP,
+    };
+    const expected = {
+      startingTime: {
+        work: 1500,
+        shortBreak: 300,
+        longBreak: 900,
+      },
+      seconds: 100,
+      active: null,
+    };
+    const actual = reducer(stateBefore(), action);
+    expect(actual).toEqual(expected);
+  });
+
+  it('handles timer tick', () => {
+    const action = {
+      type: TICK,
+    };
+    const expected = {
+      startingTime: {
+        work: 1500,
+        shortBreak: 300,
+        longBreak: 900,
+      },
+      seconds: 99,
+    };
+    const actual = reducer(stateBefore(), action);
+    expect(actual).toEqual(expected);
+  });
+
+  it('handles toggling settings', () => {
+    const action = {
+      type: TOGGLE_SETTINGS,
+    };
+    const expected = {
+      startingTime: {
+        work: 1500,
+        shortBreak: 300,
+        longBreak: 900,
+      },
+      seconds: 100,
+      settingsVisibility: true,
+    };
+    const actual = reducer(stateBefore(), action);
+    expect(actual).toEqual(expected);
+  });
+});
